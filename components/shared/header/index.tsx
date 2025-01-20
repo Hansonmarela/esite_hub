23178baadca9c5a6f8d043e6f88b9984b98d2bb2
Hansonmarela <selasikofihamenu@gmail.com>
2025-01-20 15:56:23 +0000

    web layout

---
+++ components/shared/header/index.tsx
@@ -0,0 +1,55 @@
+import { APP_NAME } from '@/lib/constants';
+import Image from 'next/image';
+import Link from 'next/link';
+import Menu from './menu';
+import { Button } from '@/components/ui/button';
+import { MenuIcon } from 'lucide-react';
+import data from '@/lib/data';
+import Search from './search';
+
+export default function Header() {
+    return (
+        <header className="bg-black text-white">
+            <div className="px-4 py-3">
+                {/* Flex container for horizontal alignment */}
+                <div className="flex items-center justify-between">
+                    {/* Logo */}
+                    <Link href="/" className="flex items-center font-extrabold text-2xl">
+                        <Image src="/icons/logo.svg" width={40} height={40} alt={`${APP_NAME} logo`} />
+                        <span className="ml-2">{APP_NAME}</span>
+                    </Link>
+
+                    {/* Search Bar */}
+                    <div className="hidden md:block flex-1 max-w-lg mx-4">
+                        <Search />
+                    </div>
+
+                    {/* Sign In and Cart Icons */}
+                    <div className="flex items-center gap-4">
+                        <Menu />
+                    </div>
+                </div>
+
+                {/* Mobile Search Bar */}
+                <div className="md:hidden mt-2">
+                    <Search />
+                </div>
+
+                {/* Category Menu */}
+                <div className="flex items-center px-3 mt-3 bg-gray-800">
+                    <Button variant="ghost" className="header-button flex items-center gap-1 text-base [&_svg]:size-6">
+                        <MenuIcon />
+                        All
+                    </Button>
+                    <div className="flex items-center flex-wrap gap-3 overflow-hidden max-h-[42px]">
+                        {data.headerMenus.map((menu) => (
+                            <Link href={menu.href} key={menu.href} className="header-button !p-2">
+                                {menu.name}
+                            </Link>
+                        ))}
+                    </div>
+                </div>
+            </div>
+        </header>
+    );
+}
---
+++ lib/data.ts
@@ -0,0 +1,34 @@
+const data = {
+    headerMenus: [
+        {
+            name: "Today's Deal",
+            href: '/search?tag=todays-deal',
+        },
+        {
+            name: "New Arrivals",
+            href: '/search?tag=new-arrivals',
+        },
+        {
+            name: "Featured Products",
+            href: '/search?tag=featured',
+        },
+        {
+            name: "Best Sellers",
+            href: '/search?tag=best-seller',
+        },
+        {
+            name: "Browsing History",
+            href: '/#brower-history',
+        },
+        {
+            name: "Customer Service",
+            href: '/page/customer-service',
+        },
+        {
+            name: "Help",
+            href: '/page/help',
+        },
+    ],
+}
+
+export default data
